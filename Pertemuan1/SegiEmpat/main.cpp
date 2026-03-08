1e7aa2cc312b1d851680486fab8a3a48477c91d1
Dzaki <dzxkyaf@gmail.com>
2026-03-09 00:32:06 +0700

    pertemuan1

---
+++ Pertemuan1/SegiEmpat/main.cpp
@@ -0,0 +1,21 @@
+#include <gl/glut.h>
+
+void SegiEmpat(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0f, 0.0f, 0.0f); // Mengatur warna menjadi merah
+    glRectf(-0.18, 0.18, 0.18, -0.18); // Menggambar segi empat (x1, y1, x2, y2)
+    glFlush();
+}
+
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(640, 480);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    glutCreateWindow("Segi Empat");
+    glutDisplayFunc(SegiEmpat);
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // Latar belakang warna biru
+    glutMainLoop();
+    return 0;
+}
